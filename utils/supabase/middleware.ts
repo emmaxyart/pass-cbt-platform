580fea2fe830ca9479f97b06fd108b8ae6fa18fd
Emmaxyart <emmanueledobor34@gmail.com>
2026-02-23 01:54:27 +0100

    fix sitemap accessibility in middleware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -47,7 +47,11 @@ export async function updateSession(request: NextRequest) {
         !request.nextUrl.pathname.startsWith('/sign-in') &&
         !request.nextUrl.pathname.startsWith('/sign-up') &&
         !request.nextUrl.pathname.startsWith('/forgot-password') &&
-        request.nextUrl.pathname !== '/'
+        request.nextUrl.pathname !== '/' &&
+        !request.nextUrl.pathname.startsWith('/syllabus') &&
+        !request.nextUrl.pathname.startsWith('/about') &&
+        request.nextUrl.pathname !== '/sitemap.xml' &&
+        request.nextUrl.pathname !== '/robots.txt'
     ) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone()
